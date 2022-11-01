56a6451cdabca9f0d1f8b8977f17ab868fb33a64
Arvind <thakurarvindkr10@gmail.com>
2022-11-01 19:18:02 +0530

    changes are made

--- ConcatenationOfArray.java
+++ ConcatenationOfArray.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 
+import javax.naming.spi.DirStateFactory.Result;
+
 public class ConcatenationOfArray{
     public static void main(String[] args) {
         int [] arr = {1,2,1};
@@ -9,6 +11,7 @@ public class ConcatenationOfArray{
 
     public static int[] getConcatenationOfArray(int [] arr){
         int [] reslt = new int[2*arr.length];
+        int n = arr.length;
         for (int i = 0; i < arr.length; i++) {
             reslt[i] = arr[i];
             reslt[i+arr.length] = arr[i];
